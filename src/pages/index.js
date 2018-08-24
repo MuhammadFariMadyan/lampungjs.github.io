e9cbd6821c727f33233e863c265c725280312d84
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-08-24 15:12:14 +0700

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Lampung JS Devs</h1>
+    <p>Selamat Datang di Official Website Lampung JS Community</p>
+    <p>Website ini butuh kontribusi kalian agar semakin informatif dan bermanfaat, Yuk Berkontribusi!</p>
+  </div>
+)
+
+export default IndexPage
